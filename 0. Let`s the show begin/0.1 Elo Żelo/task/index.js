22cbb2571570dc9e1f000aee75bf7fc3af158aa7
Kosmalski Wojciech <jawmen999@gmail.com>
2024-08-07 00:26:11 +0200

    feat:first task solution

--- 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
+++ 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
@@ -5,3 +5,23 @@
 // wypisz w pętli Elo Żelo
 
 // zapisz w pliku odpowiednią ilośc Elo Żelo
+const fs = require('fs');
+const minutes = new Date().getMinutes()
+const str = "Elo żelo"
+let content = ""
+const filename = "Elo żelo.txt"
+
+for (let i = 0; i < minutes; i++) {
+    content += str + "\n"
+}
+
+console.log(content)
+
+fs.writeFile(filename, content, err => {
+    if (err) {
+        console.error(err);
+    } else {
+        // file written successfully
+    }
+})
+
