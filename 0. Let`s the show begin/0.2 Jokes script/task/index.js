d87a1a4444c5bc17afd233ecd71e573488b607e4
Kosmalski Wojciech <jawmen999@gmail.com>
2024-08-08 01:00:44 +0200

    feat:2nd optional tasks solutions

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -1,31 +1,60 @@
-// załaduj biblotekę node:readline do obsługi pobierania danych z konsoli
-// załaduj biblotekę one-liner-joke, którą wcześniej zainstalował*ś
-
-// utwórz interfejs, za pomocą którego będziesz mógł prowadzić interakcję z cli (terminalem)
-
-// używając interfejsu zadaj pytanie o kategorię żartu
-// zweryfikuj czy dane zgadzają się
-
-// używając getRandomJokeWithTag z wcześniej zaimportowanej biblioteki pobierz i wyświetl żart
-const oneLinerJokes = require('one-liner-joke')
-const readline = require('node:readline')
+import oneLinerJoke from 'one-liner-joke';
+import readline from 'node:readline';
+import { promisify } from 'node:util';
 
 
-const tags = ['animal', 'car', 'men', 'women', 'life', 'sport', 'sarcastic']
-
+const tags = ['animal', 'car', 'men', 'women', 'life', 'sport', 'sarcastic'];
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+//utrzymywaine contextu this to dla mnie cos nowego
+const questionProm = promisify(rl.question).bind(rl);
+
+/**
+ * Prompts the user for a joke category and returns their choice
+ * @async
+ * @returns {Promise<string>} A promise that resolves to the user's chosen category in lowercase
+ */
+async function getUserTag() {
+    const tag = await questionProm(`Joke from what category would you like to hear?\nCategories: ${tags}\nYour choice: `);
+    return tag.toLowerCase();
+}
 
 
-rl.question(`Joke from what category would you like to hear?\nCategories: ${tags}\nYour choice: `, tag => {
-    tag = tag.toLowerCase()
-    if (!tags.includes(tag)) {
-        console.log(`${tag} not available in categories: ${tags}`)
+/**
+ * Validates if the given tag is in the list of available categories
+ * @param {string} tag - The tag to validate
+ * @returns {boolean} True if the tag is valid, false otherwise
+ */
+function validateUserTag(tag) {
+    return tags.includes(tag);
+}
+
+
+/**
+ * Generates a random joke from the given category
+ * @param {string} tag - The category of the joke
+ * @returns {string} The body of the generated joke
+ */
+function generateJoke(tag) {
+    return oneLinerJoke.getRandomJokeWithTag(tag).body;
+}
+
+/**
+ * Main function to run the joke program
+ * @async
+ */
+async function main() {
+    let tag = await getUserTag();
+
+    if (validateUserTag(tag)) {
+        console.log(`Here's your joke:\n${generateJoke(tag)}`);
     } else {
-        const joke = oneLinerJokes.getRandomJokeWithTag(tag)
-        console.log(`Here's your joke:\n${joke.body}`)
-    }
-    rl.close()
-})
+        console.log(`${tag} not available in categories: ${tags}`);
+    };
+    rl.close();
+}
+
+main();
+
