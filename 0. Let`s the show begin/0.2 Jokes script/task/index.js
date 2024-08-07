cd54909f979167478eb811d0c46391fbb6f99fd6
Kosmalski Wojciech <jawmen999@gmail.com>
2024-08-07 23:57:36 +0200

    feat:second task solution

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -7,3 +7,25 @@
 // zweryfikuj czy dane zgadzają się
 
 // używając getRandomJokeWithTag z wcześniej zaimportowanej biblioteki pobierz i wyświetl żart
+const oneLinerJokes = require('one-liner-joke')
+const readline = require('node:readline')
+
+
+const tags = ['animal', 'car', 'men', 'women', 'life', 'sport', 'sarcastic']
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+rl.question(`Joke from what category would you like to hear?\nCategories: ${tags}\nYour choice: `, tag => {
+    tag = tag.toLowerCase()
+    if (!tags.includes(tag)) {
+        console.log(`${tag} not available in categories: ${tags}`)
+    } else {
+        const joke = oneLinerJokes.getRandomJokeWithTag(tag)
+        console.log(`Here's your joke:\n${joke.body}`)
+    }
+    rl.close()
+})
