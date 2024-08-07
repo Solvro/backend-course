dc143d087cf88eda6c40f31db324b614cc05d4ba
Tomasz Trela <153635094+TTomaszPWR@users.noreply.github.com>
2024-08-07 18:40:59 +0200

    feat: chapter 0 - task 0.1

--- 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
+++ 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
@@ -1,7 +1,9 @@
 // pobierz bibliotekę do obsługi plików
-
+const fs = require('node:fs');
 // zapisz aktualną ilość minut używając klasy Date
-
+const minutes = (new Date).getMinutes()
+const content = "Elo żelo \n".repeat(minutes)
 // wypisz w pętli Elo Żelo
-
+fs.writeFileSync("./Elo żelo.txt", content)
+console.log(content)
 // zapisz w pliku odpowiednią ilośc Elo Żelo
