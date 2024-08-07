b6d7162095990c79022a1c7f283c7b611b867041
Tomasz Trela <153635094+TTomaszPWR@users.noreply.github.com>
2024-08-08 00:05:29 +0200

    refactor: extra task 2

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -1,18 +1,18 @@
 import readline from 'node:readline'
 import oneLinerJoke from 'one-liner-joke'
-
+import { promisify } from 'util'
 
 let rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.question("Joke from what category would you like to hear? :", (tag) => {
-    const allowedCategories = ["animal", "car", "men", "women", "life", "sport", "sarcastic"]
-    
-    if(!allowedCategories.includes(tag)){
-        console.error(`${tag} is not correct joke category (${allowedCategories.join(', ')})`)
-        process.exit()
-    }
-    rl.close()
+const question = promisify(rl.question).bind(rl)
 
-    const randomJoke = oneLinerJoke.getRandomJokeWithTag(tag)
-    console.log(`Ok, here is the joke: ${randomJoke.body}`);
-});
+const tag = await question("Joke from what category would you like to hear? : ")
+const allowedCategories = ["animal", "car", "men", "women", "life", "sport", "sarcastic"]
+if (!allowedCategories.includes(tag)) {
+    console.error(`${tag} is not correct joke category (${allowedCategories.join(', ')})`)
+    process.exit()
+}
+rl.close();
+
+const randomJoke = oneLinerJoke.getRandomJokeWithTag(tag)
+console.log(`Ok, here is the joke: ${randomJoke.body}`)
