26aa571353062d2b6dde915796e71d5ae9160495
Jakub Czajkowski <and.jj.an@gmail.com>
2024-08-08 01:26:43 +0200

    refactor: divide code into functions

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -2,20 +2,45 @@ import readline from 'readline';
 import oneLinerJoke from 'one-liner-joke';
 import {promisify} from 'util';
 
-const allowedCategories = ["animal", "car", "men", "women", "life", "sport", "sarcastic"];
+/**
+ * Reads joke category from console and validates it
+ * @returns {Promise<string>} jokeCategory - validated joke category
+ */
+async function getValidJokeCategory() {
+    const allowedCategories = ["animal", "car", "men", "women", "life", "sport", "sarcastic"];
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-const question = promisify(rl.question)
-const jokeCategory = await question("Joke from what category would you like to hear? : ")
+    const question = promisify(rl.question).bind(rl);
+    const jokeCategory = await question("Joke from what category would you like to hear? : ")
+    if (!allowedCategories.includes(jokeCategory)) {
+        console.log(`${jokeCategory} is not correct joke category (${allowedCategories.join(', ')})`);
+        process.exit();
+    }
+    rl.close();
 
-if (!allowedCategories.includes(jokeCategory)) {
-    console.log(`${jokeCategory} is not correct joke category (${allowedCategories.join(', ')})`);
-} else {
+    return jokeCategory;
+}
+
+/**
+ * Using oneLinerJoke to generate and print joke from the right category
+ * @param {string} jokeCategory - accepted category
+ */
+function getAndDisplayJoke(jokeCategory) {
     const joke = oneLinerJoke.getRandomJokeWithTag(jokeCategory);
     console.log(`Ok, here is the joke: ${joke.body}`);
 }
-rl.close();
+
+/**
+ * Main function.
+ * Gets joke category from console to generate and display suitable joke.
+ */
+async function main() {
+    const jokeCategory = await getValidJokeCategory();
+    getAndDisplayJoke(jokeCategory);
+}
+
+await main()
