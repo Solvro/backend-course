716874278efba2dc99e81f30462303b2bb3d3aef
Jakub Czajkowski <and.jj.an@gmail.com>
2024-08-08 00:29:43 +0200

    feat: change callback to promise

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -1,5 +1,6 @@
 import readline from 'readline';
 import oneLinerJoke from 'one-liner-joke';
+import {promisify} from 'util';
 
 const allowedCategories = ["animal", "car", "men", "women", "life", "sport", "sarcastic"];
 
@@ -8,12 +9,13 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-rl.question("Joke from what category would you like to hear? : ", jokeCategory => {
-    if (!allowedCategories.includes(jokeCategory)) {
-        console.log(`${jokeCategory} is not correct joke category (${allowedCategories.join(', ')})`);
-    } else {
-        const joke = oneLinerJoke.getRandomJokeWithTag(jokeCategory);
-        console.log(`Ok, here is the joke: ${joke.body}`);
-    }
-    rl.close();
-});
+const question = promisify(rl.question)
+const jokeCategory = await question("Joke from what category would you like to hear? : ")
+
+if (!allowedCategories.includes(jokeCategory)) {
+    console.log(`${jokeCategory} is not correct joke category (${allowedCategories.join(', ')})`);
+} else {
+    const joke = oneLinerJoke.getRandomJokeWithTag(jokeCategory);
+    console.log(`Ok, here is the joke: ${joke.body}`);
+}
+rl.close();
