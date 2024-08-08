364fa018c9f6476bd21fcfcd3077505fe77c05e7
Jakub Czajkowski <and.jj.an@gmail.com>
2024-08-08 02:11:06 +0200

    feat: student type and email address generation

--- 0. Let`s the show begin/0.3 TypeScript/task/index.ts
+++ 0. Let`s the show begin/0.3 TypeScript/task/index.ts
@@ -1,9 +1,17 @@
-// utwórz typ Studenta
+type Student = {
+    firstName: string;
+    lastName: string;
+    index: number;
+}
 
-// utwórz funkcję generującą email studenta na podstawie indexu
+function generateEmailForStudent(student: Student) {
+    return `${student.index}@student.pwr.edu.pl`;
+}
 
-// utwórz obiekt studenta wraz z typem
+const student :Student = {
+    firstName: "John",
+    lastName: "Doe",
+    index: 213742
+};
 
-// wygeneruj i wyświetl email dla studenta
-
-// skompiluj i uruchom kod
+console.log(generateEmailForStudent(student));
