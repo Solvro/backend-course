f5ba8d58d28ee37e647cb4c334981ccd7b3d8a66
Mateusz Płaska <plaskamateuszi8@gmail.com>
2024-08-09 20:10:09 +0200

    feat: add task 2

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -7,3 +7,24 @@
 // zweryfikuj czy dane zgadzają się
 
 // używając getRandomJokeWithTag z wcześniej zaimportowanej biblioteki pobierz i wyświetl żart
+
+const readline = require('readline');
+const oneLinerJoke = require('one-liner-joke');
+
+const cli = readline.createInterface({
+    input:  process.stdin,
+    output: process.stdout,
+});
+
+cli.question("Joke from what category would you like to hear? : ", (givenJokeTag) => {
+    const availableTags = ["animal", "car", "men", "women", "life", "sport", "sarcastic"];
+    
+    if (!availableTags.includes(givenJokeTag)) {
+        console.error(`${givenJokeTag} is not correct joke category (${availableTags.join(',')})`);
+        process.exit();
+    }
+    cli.close();
+
+    const joke = oneLinerJoke.getRandomJokeWithTag(givenJokeTag);
+    console.log(`Ok, here is the joke: ${joke.body}`);
+});
