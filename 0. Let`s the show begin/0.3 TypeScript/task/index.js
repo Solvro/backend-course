f0713b30f2e34cdf5fac8ac4e0ba1042c14b858a
Tomasz Trela <153635094+TTomaszPWR@users.noreply.github.com>
2024-08-09 21:51:49 +0200

    chapter 0 - task 0.3

--- 0. Let`s the show begin/0.3 TypeScript/task/index.js
+++ 0. Let`s the show begin/0.3 TypeScript/task/index.js
@@ -1,20 +1,13 @@
-// utwórz typ Studenta
-type Student = {
-    firstName: string;
-    lastName: string;
-    index: number;
-}
 // utwórz funkcję generującą email studenta na podstawie indexu
-function generateEmailFromStudent(student: Student){
-    return `${student.index}@student.pwr.edu.pl`
+function generateEmailFromStudent(student) {
+    return "".concat(student.index, "@student.pwr.edu.pl");
 }
 // utwórz obiekt studenta wraz z typem
-const student: Student = {
+var student = {
     firstName: "Tomasz",
     lastName: "Trela",
     index: 280598
-}
+};
 // wygeneruj i wyświetl email dla studenta
 console.log(generateEmailFromStudent(student));
-
 // skompiluj i uruchom kod
