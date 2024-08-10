7c4e8e45b365fb5f03ea75752aa6b3343018165b
Bartix3021 <62841010+Bartix3021@users.noreply.github.com>
2024-08-10 15:59:54 +0200

    feat:task-2-extra-2

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -1,22 +1,28 @@
 // załaduj biblotekę node:readline do obsługi pobierania danych z konsoli
 // załaduj biblotekę one-liner-joke, którą wcześniej zainstalował*ś
 import joke from "one-liner-joke"
+import util from "util";
 import readline from "node:readline";
+import { get } from "http";
 // utwórz interfejs, za pomocą którego będziesz mógł prowadzić interakcję z cli (terminalem)
 const i = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 const tags = ["animal", "car", "men", "women", "life", "sport", "sarcastic"]
-i.question("Jaka kategoria żartu wariacie: ", (res) => {
-    let r = joke.getRandomJokeWithTag(res).body
-    if (!tags.includes(res)) {
+const get_data = util.promisify(i.question).bind(i)
+get_data("Jaka kategoria żartu wariacie: ").then(data => {
+    let r = joke.getRandomJokeWithTag(data).body
+    if (!tags.includes(data)) {
         console.log("Niewłaściwa kategoria")
     } else {
         console.log(r)
     }
     i.close()
+}).catch(err => {
+    console.error(err)
 })
+
 // używając interfejsu zadaj pytanie o kategorię żartu
 // zweryfikuj czy dane zgadzają się
 
