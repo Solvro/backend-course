dfc2024d7ad4a628d39e60d18bba4fc2da124631
Bartix3021 <62841010+Bartix3021@users.noreply.github.com>
2024-08-10 16:50:27 +0200

    feat:task-3

---
+++ 0. Let`s the show begin/0.3 TypeScript/task/index.js
@@ -0,0 +1,14 @@
+function getEmail(s) {
+    return "".concat(s.index, "@student.pwr.edu.pl");
+}
+var samplestudent = {
+    firstName: "Bartek",
+    lastName: "Fopke",
+    index: 284241
+};
+console.log(getEmail(samplestudent));
+// utwórz typ Studenta
+// utwórz funkcję generującą email studenta na podstawie indexu
+// utwórz obiekt studenta wraz z typem
+// wygeneruj i wyświetl email dla studenta
+// skompiluj i uruchom kod
--- 0. Let`s the show begin/0.3 TypeScript/task/index.ts
+++ 0. Let`s the show begin/0.3 TypeScript/task/index.ts
@@ -1,3 +1,22 @@
+type Student = {
+    firstName:string,
+    lastName:string,
+    index:Number
+
+}
+
+function getEmail(s : Student) {
+    return `${s.index}@student.pwr.edu.pl`;
+}
+
+const samplestudent : Student = {
+    firstName: "Bartek",
+    lastName: "Fopke",
+    index: 284241
+}
+
+console.log(getEmail(samplestudent))
+
 // utwórz typ Studenta
 
 // utwórz funkcję generującą email studenta na podstawie indexu
