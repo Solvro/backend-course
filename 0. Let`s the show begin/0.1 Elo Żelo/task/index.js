52185ea6e65a77f36ad277affeeaed9909ce1511
Bartix3021 <62841010+Bartix3021@users.noreply.github.com>
2024-08-10 15:13:49 +0200

    feat:task_1

--- 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
+++ 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
@@ -1,7 +1,16 @@
 // pobierz bibliotekę do obsługi plików
-
+const files = require("fs");
 // zapisz aktualną ilość minut używając klasy Date
-
+let data = new Date();
+let min = data.getMinutes()
+console.log(data.getMinutes())
 // wypisz w pętli Elo Żelo
-
+let content = "";
+for (let i = 0; i < min; i++) {
+    console.log("Elo Żelo");
+    content += "Elo Żelo\n";
+}
 // zapisz w pliku odpowiednią ilośc Elo Żelo
+files.writeFile("Elo żelo.txt", content, (err) => {
+    if (err) throw err;
+})
