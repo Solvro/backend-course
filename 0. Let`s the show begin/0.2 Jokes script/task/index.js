f82e2cb1c4f7558ce1741fa04daf7558c8718a7a
HETTski <tanski.hubo@gmail.com>
2024-08-11 21:07:44 +0200

    completed task 2

--- 0. Let`s the show begin/0.2 Jokes script/task/index.js
+++ 0. Let`s the show begin/0.2 Jokes script/task/index.js
@@ -1,9 +1,25 @@
 // załaduj biblotekę node:readline do obsługi pobierania danych z konsoli
+const readline = require('node:readline');
 // załaduj biblotekę one-liner-joke, którą wcześniej zainstalował*ś
+const oneLinerJoke = require('one-liner-joke');
 
 // utwórz interfejs, za pomocą którego będziesz mógł prowadzić interakcję z cli (terminalem)
-
+const cli = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+const allowedCategories =['animal', 'car', 'men', 'women', 'life','sport','sarcastic']
 // używając interfejsu zadaj pytanie o kategorię żartu
+cli.question("Choose a joke category you would like to hear sir \n allowed categories are: " + allowedCategories + '\n',jokeCategory =>{
+    if (!allowedCategories.includes(jokeCategory)){
+        console.error('this is not correct category!')
+        process.exit()
+    }
+    cli.close();
+    const joke = oneLinerJoke.getRandomJokeWithTag(jokeCategory)
+    console.log(`your super funny joke is: \n ${joke.body}`)
+})
+
 // zweryfikuj czy dane zgadzają się
 
 // używając getRandomJokeWithTag z wcześniej zaimportowanej biblioteki pobierz i wyświetl żart
