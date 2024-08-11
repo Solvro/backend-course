b1835a6311011a1849da365687e4691a0b8d4a12
HETTski <tanski.hubo@gmail.com>
2024-08-11 17:12:37 +0200

    excercise 0.1 done

--- 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
+++ 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
@@ -1,7 +1,24 @@
 // pobierz bibliotekę do obsługi plików
 
+const fs = require('fs');
+
+
 // zapisz aktualną ilość minut używając klasy Date
 
+const numberOfMinutes = (new Date()).getMinutes();
+
+
 // wypisz w pętli Elo Żelo
+const message = "Eloo Żeeelooo"
+
+for(let i = 0; i< numberOfMinutes; i++){
+    console.log(message)
+}
 
 // zapisz w pliku odpowiednią ilośc Elo Żelo
+
+    fs.writeFile('eloŻelo.txt', (message + '\n').repeat(numberOfMinutes) , err =>{
+        if(err){
+            console.error(err)
+        }
+    })
