4d3561c3ff0565462d15ade7c442076d9ade16f6
Versim <wojciechkrzos67@gmail.com>
2024-08-13 10:03:06 +0200

    feat: finish 0.1

--- 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
+++ 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
@@ -1,7 +1,19 @@
 // pobierz bibliotekę do obsługi plików
+var fs = require('node:fs');
 
 // zapisz aktualną ilość minut używając klasy Date
+var date = new Date();
+var minutes = date.getMinutes();
 
 // wypisz w pętli Elo Żelo
+const ELO = 'Elo of the Żelo';
+
+for (var i = 0; i < 11; i++){
+    console.log(ELO);
+}
 
 // zapisz w pliku odpowiednią ilośc Elo Żelo
+const ELO_FILE_PATH = '0. Let`s the show begin/0.1 Elo Żelo/task/';
+const ELO_FILE_NAME = 'elo.txt';
+
+fs.writeFileSync(ELO_FILE_PATH + ELO_FILE_NAME, (ELO + '\n').repeat(10));
