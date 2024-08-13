470dffc9b04fa372a7f7d43634e603cb9f7cc72a
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-08-13 12:09:11 +0200

    feat: first task

--- 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
+++ 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
@@ -5,3 +5,22 @@
 // wypisz w pętli Elo Żelo
 
 // zapisz w pliku odpowiednią ilośc Elo Żelo
+
+const fs = require("node:fs");
+const range = new Date().getMinutes();
+
+const getEloZelo = () => {
+  let content = "";
+  for (let i = 0; i < range; i++) {
+    content += "Elo żelo \n";
+  }
+  return content;
+};
+
+const output = getEloZelo();
+
+fs.writeFile("Elo żelo.txt", output, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
