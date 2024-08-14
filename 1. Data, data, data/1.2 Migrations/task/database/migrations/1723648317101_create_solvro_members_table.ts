9560342c177791b5026816a42ba9f99e4de0eec6
Kosmalski Wojciech <jawmen999@gmail.com>
2024-08-14 22:33:21 +0200

    fix: delete type in db problem

--- 1. Data, data, data/1.2 Migrations/task/database/migrations/1723648317101_create_solvro_members_table.ts
+++ 1. Data, data, data/1.2 Migrations/task/database/migrations/1723648317101_create_solvro_members_table.ts
@@ -10,7 +10,8 @@ export default class extends BaseSchema {
       table.string('last_name', 100).notNullable()
       table.enum('status', ['Active', 'Inactive', 'Trainee', 'Honorary'], {
         useNative: true,
-        enumName: 'member_status'
+        enumName: 'member_status',
+        existingType: false
       }).nullable()
     })
   }
@@ -20,7 +21,9 @@ export default class extends BaseSchema {
     //okay - lack of semicolon at the end was the problem...
     //interesting, because in docs there is no semicolon
     //https://lucid.adonisjs.com/docs/table-builder#enum--enu
-    this.schema.raw('DROP TYPE IF EXISTS "member_status";')
+    //UPDATE: semicolon wasn't the problem, idk what is, but raw SQL doesnt work
+    //        so I need to run migration:fresh with --drop-types flag
+    this.schema.raw('DROP TYPE IF EXISTS "member_status"')
     this.schema.dropTable(this.tableName)
   }
 }
