426ce621b5701662d1b2f185220e9cf1755388e7
Skiperpol <skiperpol1@gmail.com>
2024-08-17 23:18:22 +0200

    fix: migration improve

--- 1. Data, data, data/1.2 Migrations/task/database/migrations/1723220403663_create_students_table.ts
+++ 1. Data, data, data/1.2 Migrations/task/database/migrations/1723220403663_create_students_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.uuid('id').primary().notNullable().unique().defaultTo(this.raw('uuid_generate_v4()'))
-      table.string('index', 255).notNullable()
+      table.string('index', 255).notNullable().unique()
       table.string('first_name', 255).notNullable()
       table.string('last_name', 255).notNullable()
       table.enu('status', ['IMPLEMENTATION', 'ACTIVE', 'HONORABLE', 'INACTIVE']).notNullable().defaultTo('IMPLEMENTATION')
