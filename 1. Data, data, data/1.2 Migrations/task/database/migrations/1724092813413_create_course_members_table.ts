53801f697160e9329af4e67167f662bb5ff24451
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-08-19 21:42:04 +0200

    exersize 1.2

---
+++ 1. Data, data, data/1.2 Migrations/task/database/migrations/1724092813413_create_course_members_table.ts
@@ -0,0 +1,18 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'course_members'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.integer('course_id').references('courses.course_id')
+      table.integer('member_id').references('members.member_id')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
