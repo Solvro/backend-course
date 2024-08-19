4bebed3132f3aa3c7d3c2c0be2aab43318a133e3
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-08-19 22:07:31 +0200

    exersize 1.3

---
+++ 1. Data, data, data/1.3 Seeders/task/database/seeders/department_seeder.ts
@@ -0,0 +1,11 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import db from '@adonisjs/lucid/services/db'
+
+export default class extends BaseSeeder {
+  async run() {
+    // Write your database queries inside the run method
+    await db.rawQuery(
+      "INSERT INTO departments (name) VALUES ('Backend'), ('Devops'), ('Frontend'), ('UI'), ('ML'), ('Mobile');"
+    )
+  }
+}
