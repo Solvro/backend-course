a0007a78d70af71dbb51fd329c902be94c79438c
Jakub Czajkowski <and.jj.an@gmail.com>
2024-08-19 15:24:35 +0200

    init: copy migrations from the previous task

---
+++ 1. Data, data, data/1.3 Seeders/task/database/migrations/1724065777161_create_courses_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'courses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+
+      table.string('name')
+
+      table.integer('department_id').references('departments.id').onDelete('RESTRICT')
+
+      table.string('resource_url').nullable()
+      table.text('description').nullable()
+
+      table.timestamp('created_at').defaultTo(this.now())
+      table.timestamp('updated_at').defaultTo(this.now())
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
