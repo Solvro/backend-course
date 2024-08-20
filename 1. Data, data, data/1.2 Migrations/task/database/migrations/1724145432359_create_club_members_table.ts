714f4b1664870c9ff63448068fc2cf02a521b41d
Mateusz Płaska <plaskamateuszi8@gmail.com>
2024-08-20 11:30:33 +0200

    feat: add task 2

---
+++ 1. Data, data, data/1.2 Migrations/task/database/migrations/1724145432359_create_club_members_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'club_members'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('index')
+
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
+      table.enum('status', ['Active', 'Inactive', 'Trainee', 'Honorary'], {
+        useNative: true,
+        enumName: 'member_status',
+        existingType: false
+      }).notNullable()
+
+      table.timestamps(true)
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
