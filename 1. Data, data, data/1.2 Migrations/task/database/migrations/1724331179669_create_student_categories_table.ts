c8f1223b4803565e13b368d415bbfec905bb48e8
HETTski <tanski.hubo@gmail.com>
2024-08-22 18:47:31 +0200

    completed excercise 2

---
+++ 1. Data, data, data/1.2 Migrations/task/database/migrations/1724331179669_create_student_categories_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'student_categories'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.integer('student_index').references('students.index').onDelete('CASCADE')
+      table.integer('category_id').references('categories.id').onDelete('RESTRICT')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ 1. Data, data, data/1.2 Migrations/task/database/migrations/1724331193186_create_courses_table.ts
@@ -0,0 +1,24 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'courses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('name').unique()
+      table.integer('category_id').references('categories.id').onDelete('CASCADE')
+      table.string('description').nullable()
+      table.string('url').nullable()
+
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
