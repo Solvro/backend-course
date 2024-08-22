fa20ca9aa0f46bca3454841c25d4b64c52976779
Kosmalski Wojciech <jawmen999@gmail.com>
2024-08-23 00:54:48 +0200

    init: copy migrations and seeders

---
+++ 1. Data, data, data/1.4 Triggers (optional)/task/database/migrations/1723649815643_create_courses_table.ts
@@ -0,0 +1,39 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'courses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name', 100)
+      table.integer('section_id').references('sections.id').notNullable()
+      table.text('description').nullable()
+    })
+
+    this.schema.raw(`
+      CREATE OR REPLACE FUNCTION slugify_resource_link () RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+        BEGIN
+            IF NEW.resource_link IS NULL THEN
+                NEW.resource_link := 'https://solvro/blog/' || 
+                                    trim(BOTH '-' FROM regexp_replace(lower(unaccent(trim(NEW.name))), '[^a-z0-9\\-_]+', '-', 'gi'));
+            END IF;
+            RETURN NEW;
+        END;
+        $$;
+      `)
+
+    this.schema.raw(`
+      CREATE TRIGGER generate_resource_link_if_null 
+      BEFORE INSERT 
+      ON courses 
+      FOR ROW
+      EXECUTE PROCEDURE slugify_resource_link ();
+      `)
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+    this.schema.raw('DROP FUNCTION slugify_resource_link() CASCADE')
+  }
+}
---
+++ 1. Data, data, data/1.4 Triggers (optional)/task/database/migrations/1723651606494_create_member_sections_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'member_sections'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.integer('member_index').references('solvro_members.index')
+      table.integer('section_id').references('sections.id')
+      table.primary(['member_index', 'section_id'])
+      table.date('joined_at').notNullable()
+      table.date('left_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
