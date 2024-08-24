0e7bf693e966e1a23ef3c017d1c383361366f8de
Dawid Linek <dawid@linek.dev>
2024-08-24 17:31:12 +0200

    feat: chapter 3 - task 3.2 Views

--- 3. Let me see/3.1 Http/solution/index.js
+++ 3. Let me see/3.1 Http/solution/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-http.createServer((response) => {
+http.createServer((request, response) => {
     const minutes = (new Date).getMinutes()
     response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
     response.write("Elo żelo \n".repeat(minutes));
