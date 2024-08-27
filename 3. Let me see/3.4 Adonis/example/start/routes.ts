9fa4f579a042164ce9c5c13236571ab92f453c74
Dawid Linek <dawid@linek.dev>
2024-08-27 18:08:07 +0200

    feature: chapter 3 - example 3.4 Adonis controllers

--- 3. Let me see/3.4 Adonis/example/start/routes.ts
+++ 3. Let me see/3.4 Adonis/example/start/routes.ts
@@ -7,29 +7,10 @@
 |
 */
 
-import Student from '#models/student'
-import { createStudentValidator } from '#validators/student'
 import router from '@adonisjs/core/services/router'
-import { DateTime } from 'luxon'
+const StudentsController = () => import('#controllers/students_controller')
 
-router.get('/', async ({ view, request }) => {
-  const page = Number(request.input('page', 1))
-  const limit = 10
-  const students = await Student.query()
-    .limit(limit)
-    .offset(limit * (page - 1))
-  return view.render('students', { students, page })
-})
-
-router.get('create', ({ view }) => view.render('create'))
-router.post('create', async ({ request, response }) => {
-  const data = await createStudentValidator.validate(request.all())
-  await Student.create({ ...data, dateOfBirth: DateTime.fromJSDate(data.dateOfBirth) })
-  return response.redirect().toRoute('/')
-})
-
-router.get('/:index', async ({ request, view }) => {
-  return view.render('student', {
-    student: await Student.findByOrFail('index', request.param('index')),
-  })
-})
+router.get('/', [StudentsController, 'index'])
+router.get('create', [StudentsController, 'create'])
+router.post('create', [StudentsController, 'store'])
+router.get('/:index', [StudentsController, 'show'])
