bfcbe2075f00a95deb711cdf7c23c56359fd5279
Versim <wojciechkrzos67@gmail.com>
2024-08-27 18:15:36 +0200

    feat(2.2, 2.3): implement models for all relations

--- 2. Look at me I’m a model/task/app/models/department.ts
+++ 2. Look at me I’m a model/task/app/models/department.ts
@@ -1,13 +1,32 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { BaseModel, column, belongsTo} from '@adonisjs/lucid/orm'
+import SolvroMember from './solvro_member.js'
 
 export default class Department extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare name: string
+
+  @column()
+  declare chairperson: number
+
+  @belongsTo(() => SolvroMember, {
+    foreignKey: 'chairperson',
+  })
+  public chairpersonMember!: BelongsTo<typeof SolvroMember> // is the '!' coorect?
+
   @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
+  declare created_at: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  declare updated_at: DateTime
+
+  // Can a constructor be used here in pair with the exclamation sign above at line 19?
+  constructor() {
+    super();
+    this.chairpersonMember = {} as BelongsTo<typeof SolvroMember>;
+  }
 }
