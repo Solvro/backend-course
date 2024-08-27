f37a38b13855640feefb70d79523ec1f2dff1bc8
D0dii <marcindolatowski123@gmail.com>
2024-08-27 15:27:05 +0200

    feat: zadanie 1

--- 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
+++ 0. Let`s the show begin/0.1 Elo Żelo/task/index.js
@@ -5,3 +5,17 @@
 // wypisz w pętli Elo Żelo
 
 // zapisz w pliku odpowiednią ilośc Elo Żelo
+const CONTENT = "Elo żelo";
+const fs = require("node:fs");
+
+const minutes = new Date().getMinutes();
+
+for (let i = 0; i < minutes; i++) {
+  console.log("Elo Żelo");
+}
+
+let messageToBeSaved = "";
+for (let i = 0; i < minutes; i++) {
+  messageToBeSaved += CONTENT + "\n";
+}
+fs.writeFile("Elo żelo.txt", messageToBeSaved, (err) => (err ? console.log(err) : ""));
