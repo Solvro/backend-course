2cf55d5b7a8e8b479874bacdd9fb69cb3c3fcac8
Mateusz Płaska <plaskamateuszi8@gmail.com>
2024-08-30 12:02:31 +0200

    fix: fix member's index possible values

--- 3. Let me see/3.4 Adonis/task/database/factories/club_member_factory.ts
+++ 3. Let me see/3.4 Adonis/task/database/factories/club_member_factory.ts
@@ -4,7 +4,7 @@ import ClubMember from '#models/club_member'
 export const ClubMemberFactory = factory
   .define(ClubMember, async ({ faker }) => {
     return {
-      index: faker.number.int({min: 1, max: 999999}),
+      index: faker.number.int({min: 100000, max: 999999}),
       firstName: faker.person.firstName(),
       lastName: faker.person.lastName(),
       status: faker.helpers.arrayElement(['Active', 'Inactive', 'Trainee', 'Honorary']) as 'Active' | 'Inactive' | 'Trainee'| 'Honorary',
