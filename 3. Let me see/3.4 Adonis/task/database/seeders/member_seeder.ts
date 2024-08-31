c5fc8aeeda36ac4bb1cb8d173c9fa36261609ed9
Jakub Czajkowski <and.jj.an@gmail.com>
2024-08-31 23:54:31 +0200

    feat: add member seeder

---
+++ 3. Let me see/3.4 Adonis/task/database/seeders/member_seeder.ts
@@ -0,0 +1,8 @@
+import {BaseSeeder} from '@adonisjs/lucid/seeders'
+import {MemberFactory} from "#database/factories/member_factory";
+
+export default class extends BaseSeeder {
+  async run() {
+    await MemberFactory.createMany(10)
+  }
+}
