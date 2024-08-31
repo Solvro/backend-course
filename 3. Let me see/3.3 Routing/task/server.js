7fadd369d760831413e1763e8d042020a2c3e17c
Kosmalski Wojciech <jawmen999@gmail.com>
2024-08-31 23:00:33 +0200

    feat: add task 3.3

---
+++ 3. Let me see/3.3 Routing/task/server.js
@@ -0,0 +1,41 @@
+import { getRandomJokeWithTag } from "./node_modules/one-liner-joke/index.js";
+import http from "http";
+import fs from "fs";
+
+http
+  .createServer((request, response) => {
+    let view = "";
+    let tag = request.url.match(/(?<=\/categories\/)\w+/)?.[0];
+    switch (true) {
+      case tag ? true : false:
+        view = handleCategoriesView(tag);
+        break;
+
+      case request.url === "/":
+        view = handleHomeView();
+        break;
+
+      default:
+        view = handleHomeView();
+        break;
+    }
+
+    response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    response.write(view);
+    response.end();
+  })
+  .listen(1234);
+
+function handleHomeView() {
+  let view = fs.readFileSync("./index.html").toString();
+  view = view.replaceAll("{{dateNow}}", new Date().toLocaleString("pl-PL"));
+  return view;
+}
+
+function handleCategoriesView(tag) {
+  let view = fs.readFileSync("./category.html").toString();
+  const joke = getRandomJokeWithTag(tag);
+  view = view.replaceAll("{{tag}}", tag);
+  view = view.replaceAll("{{joke}}", joke.body);
+  return view;
+}
