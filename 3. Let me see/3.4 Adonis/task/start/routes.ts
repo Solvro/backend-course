1e12abe71d722b1f1f00c13af7a381ddf6cee88e
Jakub Czajkowski <and.jj.an@gmail.com>
2024-09-01 16:35:23 +0200

    feat: add member details view

--- 3. Let me see/3.4 Adonis/task/start/routes.ts
+++ 3. Let me see/3.4 Adonis/task/start/routes.ts
@@ -13,3 +13,9 @@ import Member from "#models/member";
 router.on('/').render('members', {
   members: await Member.all()
 })
+
+router.get('/:index', async ({ request, view }) => {
+  const index = request.param('index')
+  const member = await Member.findByOrFail('index', index)
+  return view.render('member_details', {member})
+})
