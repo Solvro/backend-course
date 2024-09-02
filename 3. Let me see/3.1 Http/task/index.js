adc68d5f7903dc6322e7997f5855fb4d108d60fe
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-09-02 12:08:00 +0200

    fix: task 3.1 polish chars

--- 3. Let me see/3.1 Http/task/index.js
+++ 3. Let me see/3.1 Http/task/index.js
@@ -1,11 +1,16 @@
-const http = require("http");
-http
-  .createServer((request, response) => {
-    const curDate = new Date();
-    const minutes = curDate.getMinutes();
-    for (let i = 0; i < minutes; i++) {
-      response.write("Elo zelo!\n");
-    }
-    response.end();
-  })
-  .listen(1234);
+const { createServer } = require("node:http");
+const hostname = "127.0.0.1";
+const port = 1234;
+const server = createServer((req, res) => {
+  const curDate = new Date();
+  const minutes = curDate.getMinutes();
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  for (let i = 0; i < minutes; i++) {
+    res.write("Elo żelo!\n");
+  }
+  res.end();
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
