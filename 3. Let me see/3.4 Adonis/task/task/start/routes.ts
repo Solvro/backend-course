dc67e46a5dfa6bafdb82bf3d74bf42d4b529664b
Kamil Fedio <kamilf827@gmail.com>
2024-09-05 23:29:53 +0200

    feature: task 4 exercise 4

--- 3. Let me see/3.4 Adonis/task/task/start/routes.ts
+++ 3. Let me see/3.4 Adonis/task/task/start/routes.ts
@@ -8,12 +8,24 @@
 */
 
 import Member from '#models/member'
+import { createMemberValidator } from '#validators/student'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async ({ view }) => {
   return view.render('members', {
     members: await Member.all(),
   })
+})
+
+router.get('create', ({ view }) => view.render('create'))
+router.post('create', async ({ request, response }) => {
+  const data = request.all()
+  const payload = await createMemberValidator.validate(data)
+  await Member.create({
+    ...payload,
+  })
+
+  return response.redirect().toRoute('/')
 })
 
 router.get('/:index', async ({ request, view }) => {
