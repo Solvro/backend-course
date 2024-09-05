8a7dfe9e29a289c2f61b63dfc2c1c4aafea5ddba
Kamil Fedio <kamilf827@gmail.com>
2024-09-05 16:50:06 +0200

    feature: task 4 exercise 3

--- 3. Let me see/3.4 Adonis/task/task/start/routes.ts
+++ 3. Let me see/3.4 Adonis/task/task/start/routes.ts
@@ -10,6 +10,14 @@
 import Member from '#models/member'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('members', {
-  members: await Member.all(),
+router.get('/', async ({ view }) => {
+  return view.render('members', {
+    members: await Member.all(),
+  })
+})
+
+router.get('/:index', async ({ request, view }) => {
+  const index = request.param('index')
+  const member = await Member.findByOrFail('index', index)
+  return view.render('member', { member })
 })
