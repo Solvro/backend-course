47881ac5dbf8247251fbfef2a17384de4551b0d3
Kosmalski Wojciech <jawmen999@gmail.com>
2024-09-08 19:00:59 +0200

    init: setup project

---
+++ 3. Let me see/3.4 Adonis/task/task-3.4/database/seeders/1_section_seeder.ts
@@ -0,0 +1,17 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import db from '@adonisjs/lucid/services/db'
+
+export default class extends BaseSeeder {
+  async run() {
+    await db.rawQuery(`
+      INSERT INTO "sections" (name)
+      VALUES 
+        ('Backend'), 
+        ('Devops'), 
+        ('Frontend'), 
+        ('UI'), 
+        ('ML'), 
+        ('Mobile');
+      `)
+  }
+}
