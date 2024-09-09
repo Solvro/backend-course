0bd6c9d58358e1450f31b36a80924152a772f44a
Jakub Czajkowski <and.jj.an@gmail.com>
2024-09-09 17:40:24 +0200

    init: copy member-related files from the previous chapter

---
+++ 4. Let`s communicate/task/app/validators/member.ts
@@ -0,0 +1,14 @@
+import vine from '@vinejs/vine'
+
+export const createMemberValidator = vine.compile(
+  vine.object({
+    index: vine
+      .number()
+      .min(100000)
+      .max(999999)
+      .unique(async (db, value) => !(await db.from('members').where('index', value).first())),
+    firstName: vine.string().maxLength(255),
+    lastName: vine.string().maxLength(255),
+    status: vine.enum(['onboarding', 'active', 'honorary', 'inactive'])
+  })
+)
---
+++ 4. Let`s communicate/task/database/factories/member_factory.ts
@@ -0,0 +1,14 @@
+import factory from '@adonisjs/lucid/factories'
+import Member from '#models/member'
+
+let currentIndex = 100000
+
+export const MemberFactory = factory
+  .define(Member, async ({faker}) => {
+    return {
+      index: currentIndex++,
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      status: faker.helpers.arrayElement(['onboarding', 'active', 'honorary', 'inactive'] as const)
+    }
+  }).build()
