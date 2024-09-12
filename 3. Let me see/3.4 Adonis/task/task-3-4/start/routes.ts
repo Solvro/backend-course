cef004fdc945bf44308112559cd4e720d502189a
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-09-12 02:02:44 +0200

    feat: finish all members view and detailed view

--- 3. Let me see/3.4 Adonis/task/task-3-4/start/routes.ts
+++ 3. Let me see/3.4 Adonis/task/task-3-4/start/routes.ts
@@ -7,6 +7,18 @@
 |
 */
 
+import Member from '#models/member'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.get('/', async ({ view }) => {
+  return view.render('pages/members', {
+    members: await Member.all(),
+  })
+})
+
+router.get('/:id', async ({ request, view }) => {
+  const id = request.param('id')
+  const member = await Member.findByOrFail('id', id)
+  const email = `${member.firstName.toLowerCase()}.${member.lastName.toLowerCase()}@solvro.pl`
+  return view.render('pages/details', { member: member, email: email })
+})
