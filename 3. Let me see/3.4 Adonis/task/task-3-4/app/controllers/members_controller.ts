7e8776d5c3e32fa4343617640842cad7f8886ff0
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-09-17 09:56:28 +0200

    feat: add pagination

--- 3. Let me see/3.4 Adonis/task/task-3-4/app/controllers/members_controller.ts
+++ 3. Let me see/3.4 Adonis/task/task-3-4/app/controllers/members_controller.ts
@@ -3,18 +3,26 @@ import { createMemberValidator } from '#validators/member'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class MembersController {
-  async index({ view }: HttpContext) {
+  async index({ request, view }: HttpContext) {
+    const page = Number(request.input('page', 1))
+    const limit = 10
+    const members = await Member.query()
+      .limit(limit)
+      .offset(limit * (page - 1))
+
     return view.render('pages/members', {
-      members: await Member.all(),
+      members: members,
+      page: page,
     })
   }
 
   async show({ request, view }: HttpContext) {
     const id = request.param('id')
     try {
+      const page = Number(request.input('page', 1))
       const member = await Member.findByOrFail('id', id)
       const email = `${member.firstName.toLowerCase()}.${member.lastName.toLowerCase()}@solvro.pl`
-      return view.render('pages/details', { member: member, email: email })
+      return view.render('pages/details', { member: member, email: email, page: page })
     } catch (err) {
       return view.render('pages/errors/not_found')
     }
