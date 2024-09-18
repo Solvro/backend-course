e4e7815920891ec85c8ddf595ecfb757689e40d4
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-09-19 01:54:21 +0200

    feat: add serialized email

--- 4. Let`s communicate/task/app/models/member.ts
+++ 4. Let`s communicate/task/app/models/member.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, computed } from '@adonisjs/lucid/orm'
 import Status from '../../contracts/enums/status.js'
 
 export default class Member extends BaseModel {
@@ -17,6 +17,11 @@ export default class Member extends BaseModel {
 
   @column()
   declare status: Status
+
+  @computed()
+  get email() {
+    return `${this.index}@solvro.pl`
+  }
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
