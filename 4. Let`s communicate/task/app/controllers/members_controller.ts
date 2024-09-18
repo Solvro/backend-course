75f881bbab49737a08673f8a3eccc8068053da02
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-09-18 23:11:33 +0200

    feat: add a member CRUD controller and resource routes

---
+++ 4. Let`s communicate/task/app/controllers/members_controller.ts
@@ -0,0 +1,80 @@
+import Member from '#models/member'
+import { createMemberValidator } from '#validators/member'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class MembersController {
+  async index({ request, response }: HttpContext) {
+    const page = Number(request.input('page', 1))
+    const limit = 10
+    const members = await Member.query()
+      .limit(limit)
+      .offset(limit * (page - 1))
+    response.json({
+      message: 'Members fetched successfully!',
+      data: members,
+    })
+  }
+
+  async store({ request, response }: HttpContext) {
+    const payload = await request.validateUsing(createMemberValidator)
+    const member = await Member.create({ ...payload })
+    response.status(201).json({
+      message: 'Member created successfully!',
+      data: member,
+    })
+  }
+
+  async show({ response, params: { id } }: HttpContext) {
+    const member = await Member.find(id)
+    if (member) {
+      response.status(200).json({
+        message: 'Member fetched successfully!',
+        data: member,
+      })
+    } else {
+      response.status(404).json({
+        message: 'Member not found',
+        id,
+      })
+    }
+  }
+
+  async update({ request, response, params: { id } }: HttpContext) {
+    const member = await Member.find(id)
+    if (member) {
+      const { firstName, lastName, status } = request.all()
+
+      member.firstName = firstName
+      member.lastName = lastName
+      member.status = status
+
+      await member.save()
+
+      response.status(200).json({
+        message: 'Member updated successfully!',
+        data: member,
+      })
+    } else {
+      response.status(404).json({
+        message: 'Member not found',
+      })
+    }
+  }
+
+  async destroy({ response, params: { id } }: HttpContext) {
+    const member = await Member.find(id)
+    if (member) {
+      await member.delete()
+
+      response.status(200).json({
+        message: 'Member deleted successfully!',
+        data: member,
+      })
+    } else {
+      response.status(404).json({
+        message: 'Member not found',
+        id,
+      })
+    }
+  }
+}
--- 4. Let`s communicate/task/start/routes.ts
+++ 4. Let`s communicate/task/start/routes.ts
@@ -7,6 +7,13 @@
 |
 */
 
+const MembersController = () => import('#controllers/members_controller')
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+// router.on('/').render('pages/home')
+
+router
+  .group(() => {
+    router.resource('members', MembersController).apiOnly()
+  })
+  .prefix('api/v1')
