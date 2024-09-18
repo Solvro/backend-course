4b2988f779cd17ea2c90728769723513b90572c5
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-09-19 01:39:55 +0200

    feat: add sorting

--- 4. Let`s communicate/task/app/controllers/members_controller.ts
+++ 4. Let`s communicate/task/app/controllers/members_controller.ts
@@ -1,21 +1,34 @@
 import Member from '#models/member'
 import { createMemberValidator } from '#validators/member'
 import type { HttpContext } from '@adonisjs/core/http'
+import { fields } from '@vinejs/vine/defaults'
 
 export default class MembersController {
+  FIELDS = ['id', 'firstName', 'lastName', 'status']
+
   async index({ request, response }: HttpContext) {
     const page = Number(request.input('page', 1))
     const perPage = Number(request.input('perPage', 10))
 
     const firstName = request.input('firstName', '')
     const lastName = request.input('lastName', '')
-    const status = String(request.input('status', ''))
+    const status = request.input('status', '')
+
+    const sort = decodeURIComponent(request.input('sort', '')).replaceAll("'", '')
 
     const membersQuery = Member.query()
 
     if (firstName) membersQuery.where('first_name', firstName)
     if (lastName) membersQuery.where('last_name', lastName)
     if (status) membersQuery.where('status', status)
+
+    if (sort) {
+      const sortDirection = sort[0] === '+' ? 'asc' : sort[0] === '-' ? 'desc' : ''
+      const fieldToSortBy = sort.substring(1)
+      if (this.FIELDS.includes(fieldToSortBy) && sortDirection) {
+        membersQuery.orderBy(fieldToSortBy, sortDirection)
+      }
+    }
 
     const members = await membersQuery.limit(perPage).offset(perPage * (page - 1))
 
