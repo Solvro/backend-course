a11f1ec9bdb906ca6be241f0d6d7e88abe59dc97
Szymczek <szymon.szymczewski@protonmail.com>
2024-10-01 15:48:47 +0200

    task 3.2

---
+++ 3. Let me see/3.2 Views/task/server.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs = require('fs');
+const path = require('path');
+const oneLinerJoke = require("one-liner-joke");
+const requestListener = (req, res) => {
+    if (req.url === '/') {
+        const now = new Date();
+        const formattedDate = now.toLocaleString('pl-PL', { dateStyle: 'full', timeStyle: 'long' });
+        const joke = oneLinerJoke.getRandomJoke().body;
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), 'utf-8', (err, content) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Błąd serwera');
+                return;
+            }
+            content = content.replace('{{date}}', formattedDate).replace('{{joke}}', joke);
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(content);
+        });
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Nie znaleziono');
+    }
+};
+const server = http_1.default.createServer(requestListener);
+const PORT = 1234;
+server.listen(PORT, () => {
+    console.log(`Serwer działa na http://localhost:${PORT}`);
+});
---
+++ 3. Let me see/3.2 Views/task/server.ts
@@ -0,0 +1,35 @@
+import http from 'http';
+const fs = require('fs');
+const path = require('path');
+const oneLinerJoke = require("one-liner-joke");
+
+const requestListener = (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.url === '/') {
+        const now = new Date();
+        const formattedDate = now.toLocaleString('pl-PL', { dateStyle: 'full', timeStyle: 'long' });
+        const joke = oneLinerJoke.getRandomJoke().body;
+
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), 'utf-8', (err: NodeJS.ErrnoException | null, content: string) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Błąd serwera');
+                return;
+            }
+
+            content = content.replace('{{date}}', formattedDate).replace('{{joke}}', joke);
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(content)
+        });
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Nie znaleziono');
+    }
+};
+
+const server = http.createServer(requestListener);
+
+const PORT = 1234;
+server.listen(PORT, () => {
+    console.log(`Serwer działa na http://localhost:${PORT}`);
+});
