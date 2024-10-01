fef8b0126589697eb2419b4533b5272286809acc
Szymczek <szymon.szymczewski@protonmail.com>
2024-10-01 18:21:34 +0200

    task 3.4 member list with styling

--- 3. Let me see/3.4 Adonis/task/task-4/start/routes.ts
+++ 3. Let me see/3.4 Adonis/task/task-4/start/routes.ts
@@ -7,6 +7,17 @@
 |
 */
 
+import Member from '#models/member'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+// router.on('/').render('members', {
+//     students: await Member.all(),
+//   })
+  
+
+router.get('/', async ({ view }) => {
+    return view.render('members', {
+        members: await Member.all(),
+    })
+})
+  
