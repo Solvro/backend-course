9a3ecc85a353609533f1049b9896693645eebaf3
Szymczek <szymon.szymczewski@protonmail.com>
2024-10-02 00:04:36 +0200

    task 3.4 create new member

--- 3. Let me see/3.4 Adonis/task/task-4/start/routes.ts
+++ 3. Let me see/3.4 Adonis/task/task-4/start/routes.ts
@@ -10,16 +10,25 @@
 import Member from '#models/member'
 import router from '@adonisjs/core/services/router'
 
-// router.on('/').render('members', {
-//     students: await Member.all(),
-//   })
-  
-
 router.get('/', async ({ view }) => {
     return view.render('members', {
         members: await Member.all(),
     })
 })
+
+router.get('/create', async ({ view }) => {
+    return view.render('create')
+})
+
+router.post('create', async ({ request, response }) => {
+    await Member.create({
+      index: request.input('index'), 
+      firstName: request.input('firstName'),
+      lastName: request.input('lastName'),
+      status: request.input('status'),
+    })
+    return response.redirect('/')
+})  
 
 router.get('/:index', async ({ request, view }) => {
     const index = request.param('index') 
@@ -27,6 +36,6 @@ router.get('/:index', async ({ request, view }) => {
     return view.render('member', {member})
 })
 
-router.on('create').render('create')
+
 
   
