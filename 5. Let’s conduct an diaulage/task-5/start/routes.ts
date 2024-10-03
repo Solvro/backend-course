ae38b395f3a5616c8ef0b730596a0d79011b0f93
Kamil Fedio <kamilf827@gmail.com>
2024-10-04 00:49:23 +0200

    feature: task two

--- 5. Let’s conduct an diaulage/task-5/start/routes.ts
+++ 5. Let’s conduct an diaulage/task-5/start/routes.ts
@@ -9,6 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import { throttle } from './limiter.js'
 const MembersController = () => import('#controllers/members_controller')
 const AuthController = () => import('#controllers/auth_controller')
 
@@ -23,6 +24,7 @@ router
     router.post('register', [AuthController, 'register']),
       router.post('login', [AuthController, 'login'])
   })
+  .use(throttle)
   .prefix('api/v1')
 
 router
